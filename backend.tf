a32fedc800259f5331fd25a02f51f0b773cde599
AayushbajajCAW <aayush.bajaj@cawstudios.com>
2023-01-20 19:28:38 +0530

    Terrafrom-Demo Branch final commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket     = "hbits-aws-aps1-demo-s3-tfstate"
-    key        = "s3-backend-hbits-core-infra-module.tfstate"
+    key        = "s3-backend-demo-infra-module.tfstate"
     region     = "ap-south-1"
   }
 }
