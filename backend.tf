9a1a7a8240b0a5639300efedef47aa2fe500eef2
AayushbajajCAW <aayush.bajaj@cawstudios.com>
2023-01-24 16:07:55 +0530

    Final Test

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket     = "hbits-aws-aps1-demo-s3-tfstate"
+    bucket     = "caw-aws-aps1-demo-s3-tfstate"
     key        = "s3-backend-demo-infra-module.tfstate"
     region     = "ap-south-1"
   }
